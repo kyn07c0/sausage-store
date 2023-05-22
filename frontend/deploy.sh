95b361ef59dab424047fada00eea198b5f62ac1f
std-017-006 std-017-006 <std-017-006@praktikum-services.ru>
2023-05-22 17:42:39 +0000

    Add new file

---
+++ frontend/deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#Если свалится одна из команд, рухнет и весь скрипт
+set -xe
+#Перезаливаем дескриптор сервиса на ВМ для деплоя
+sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
+sudo rm -f /home/front-user/sausage-store.tar.gz||true
+#Переносим артефакт в нужную папку
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPO_URL}/sausage-store-kryvinya-yuriy-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
+tar -xf ./archive.tar.gz -C /var/www-data/
+#Обновляем конфиг systemd с помощью рестарта
+sudo systemctl daemon-reload
+#Перезапускаем сервис сосисочной
+sudo systemctl restart sausage-store-frontend
