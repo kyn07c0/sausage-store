e0e9b7ffa1d51d98d9fa82bce2b3c0d6df2c8bb3
std-017-006 std-017-006 <std-017-006@praktikum-services.ru>
2023-05-25 10:58:08 +0000

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -6,8 +6,8 @@ sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-fro
 
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPO_URL}/sausage-store-kryvinya-yuriy-frontend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
 
-sudo rm -rf /var/www-data/frontend||true
-sudo tar -xf ./sausage-store.tar.gz -C /var/www-data/
+sudo rm -rf /var/www-data/dist/frontend||true
+sudo tar -xf ./sausage-store.tar.gz -C /var/www-data/dist/
 
 sudo systemctl daemon-reload
 sudo systemctl restart sausage-store-frontend
