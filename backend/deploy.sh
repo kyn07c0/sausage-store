f8a026e95f6f4925d06d8d42533f6b9aaae06f40
std-017-006 std-017-006 <std-017-006@praktikum-services.ru>
2023-05-25 10:39:00 +0000

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -8,7 +8,6 @@ curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_
 
 sudo rm -f /var/jarservice/sausage-store.jar||true
 sudo install -o ${BACKEND_USER} ./sausage-store.jar /var/jarservice/sausage-store.jar||true
-#cp ./sausage-store.jar /var/jarservice/sausage-store.jar||true
 
 sudo systemctl daemon-reload
 sudo systemctl restart sausage-store-backend
