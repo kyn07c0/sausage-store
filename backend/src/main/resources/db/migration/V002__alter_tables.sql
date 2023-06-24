e1f16964aa9aba6ff86b447ba6a2a10496958bfa
std-017-006 std-017-006 <std-017-006@praktikum-services.ru>
2023-06-24 10:53:21 +0000

    Update V002__alter_tables.sql

--- backend/src/main/resources/db/migration/V002__alter_tables.sql
+++ backend/src/main/resources/db/migration/V002__alter_tables.sql
@@ -1,3 +1,3 @@
-ALTER TABLE public.product ALTER COLUMN name TYPE varchar(22);
+ALTER TABLE public.product ALTER COLUMN name TYPE varchar(20);
 
 ALTER TABLE public.product ALTER COLUMN picture_url TYPE varchar(100);
