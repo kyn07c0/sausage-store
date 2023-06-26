8104829c7b9b83c770e5b2ce3d50b2e5388a98fe
std-017-006 std-017-006 <std-017-006@praktikum-services.ru>
2023-06-26 12:20:27 +0000

    Update V003__create_indexes.sql

--- backend/src/main/resources/db/migration/V003__create_indexes.sql
+++ backend/src/main/resources/db/migration/V003__create_indexes.sql
@@ -1,7 +1,7 @@
-CREATE UNIQUE INDEX idx_product_id ON product (id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_product_id ON product (id);
 
-CREATE UNIQUE INDEX idx_orders_id ON orders (id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_orders_id ON orders (id);
 
-CREATE UNIQUE INDEX idx_order_product_order_id ON order_product (order_id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_order_product_order_id ON order_product (order_id);
 
-CREATE INDEX idx_order_product_product_id ON order_product (product_id);
+CREATE INDEX IF NOT EXISTS idx_order_product_product_id ON order_product (product_id);
