98215a70531fc9ea693a83a46be59173c85f0387
std-017-006 <kyn07c0@yandex.ru>
2023-07-10 12:19:34 +0000

    edit Dockerfiles

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -5,7 +5,7 @@ docker pull gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/sausage-
 docker stop frontend || true
 docker rm frontend || true
 set -e
-docker run --rm -d --name frontend \	
+docker run -d --name frontend \	
     --network=sausage_network \
     --restart always \
     --pull always \
