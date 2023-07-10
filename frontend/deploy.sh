ba3b17ca350e50c2a46a1379c58a9b288df08953
std-017-006 <kyn07c0@yandex.ru>
2023-07-10 14:25:21 +0000

    edit Dockerfiles

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +xe
 docker network create -d bridge sausage_network || true
-docker login gitlab.praktikum-services.ru:5050 -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CIREGISTRY 
+#docker login gitlab.praktikum-services.ru:5050 -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CIREGISTRY 
 docker pull gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/sausage-frontend:latest
 docker stop frontend || true
 docker rm frontend || true
