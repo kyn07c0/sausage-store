2566d9219ea3d334edf728c03974da1815050ee0
std-017-006 <kyn07c0@yandex.ru>
2023-07-11 08:43:23 +0000

    edit Dockerfiles

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -8,8 +8,8 @@ SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
 EOF
 docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/sausage-backend:latest
-docker stop backend || true
-docker rm backend || true
+docker stop sausage-backend || true
+docker rm sausage-backend || true
 set -e
 docker run -d --name sausage-backend \
     --network=sausage_network \
