c66c6e4119da619848a1bf875eade1fec92bce46
std-017-006 <kyn07c0@yandex.ru>
2023-07-19 12:30:40 +0000

    Edit .gitlab-ci.yml

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set +xe
 docker network create -d bridge sausage_network || true
-docker pull gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/sausage-frontend:latest
-docker stop sausage-frontend || true
-docker rm sausage-frontend || true
+docker pull gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/frontend:latest
+docker stop frontend || true
+docker rm frontend || true
 set -e
-docker run -d --name sausage-frontend \
+docker run -d --name frontend \
 	--network=sausage_network \
 	--restart always \
 	--pull always \
 	-p 8080:80 \
-	gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/sausage-frontend:latest
+	gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/frontend:latest
