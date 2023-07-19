058ccaeb536cb08b3cfe1bad144a6838706c4b53
std-017-006 <kyn07c0@yandex.ru>
2023-07-19 12:55:34 +0000

    Edit .gitlab-ci.yml

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -7,13 +7,13 @@ SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
 EOF
 docker network create -d bridge sausage_network || true
-docker pull gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/backend:latest
-docker stop backend || true
-docker rm backend || true
+docker pull gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/sausage-backend:latest
+docker stop sausage-backend || true
+docker rm sausage-backend || true
 set -e
-docker run -d --name backend \
+docker run -d --name sausage-backend \
     --network=sausage_network \
     --restart always \
     --pull always \
     --env-file .env \
-    gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/backend:latest
+    gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/sausage-backend:latest
