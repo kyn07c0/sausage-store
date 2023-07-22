1a7a35a2ccbae38975ab8a76a6217d901f213779
std-017-006 <kyn07c0@yandex.ru>
2023-07-22 17:34:14 +0000

    edit gitlab-ci.yml

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 set +xe
 docker-compose up --detach --build frontend || true
-
-#docker network create -d bridge sausage_network || true
-#docker pull gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/sausage-frontend:latest
-#docker stop sausage-frontend || true
-#docker rm sausage-frontend || true
-#set -e
-#docker run -d --name sausage-frontend \
-#	--network=sausage_network \
-#	--restart always \
-#	--pull always \
-#	-p 8080:80 \
-#	gitlab.praktikum-services.ru:5050/std-017-006/sausage-store/sausage-frontend:latest
