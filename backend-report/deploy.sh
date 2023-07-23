407ca39f2edc1886e650cc858a038ac59d4a32ce
std-017-006 <kyn07c0@yandex.ru>
2023-07-23 09:37:20 +0000

    edit gitlab-ci.yml

--- backend-report/deploy.sh
+++ backend-report/deploy.sh
@@ -6,5 +6,4 @@ EOF
 docker-compose stop backend-report || true
 docker-compose rm -f backend-report || true
 docker-compose pull backend-report || true
-docker-compose --env-file .env_backend_report  up -d backend-report || true
-
+docker-compose --env-file .env_backend_report up -d backend-report || true
