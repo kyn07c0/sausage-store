e5d835cbbb4e6f3448a248af6e08c6136a3e3c1b
std-017-006 <kyn07c0@yandex.ru>
2023-07-23 19:57:20 +0000

    edit gitlab-ci.yml

--- backend-report/deploy.sh
+++ backend-report/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set +e
-cat > .env_backend_report <<EOF
-BACKEND_REPORT_VERSION=${BACKEND_REPORT_VERSION}
-DB=${SPRING_DATA_MONGODB_URI}
-EOF
+#cat > .env_backend_report <<EOF
+#BACKEND_REPORT_VERSION=${BACKEND_REPORT_VERSION}
+#DB=${SPRING_DATA_MONGODB_URI}
+#EOF
 docker-compose stop backend-report || true
 docker-compose rm -f backend-report || true
 docker-compose pull backend-report || true
