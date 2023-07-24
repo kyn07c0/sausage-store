080de64e7413526c9c21b3eb2a9f2567b2f10a59
std-017-006 <kyn07c0@yandex.ru>
2023-07-24 07:29:43 +0000

    edit gitlab-ci.yml

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,7 +3,8 @@ set +e
 cat > .env_frontend <<EOF
 FRONTEND_VERSION=${FRONTEND_VERSION}
 EOF
-docker-compose stop frontend || true
-docker-compose rm -f frontend || true
+#docker-compose stop frontend || true
+#docker-compose rm -f frontend || true
 docker-compose pull frontend || true
-docker-compose up -d frontend || true
+docker-compose up --force-recreate -d front || true
+#docker-compose up -d frontend || true
