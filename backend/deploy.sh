353e5c233cf24bf759edc8c30aa3147a288729ef
std-017-006 <kyn07c0@yandex.ru>
2023-07-26 17:57:57 +0000

    edit gitlab-ci.yml

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -6,5 +6,7 @@ SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}
 SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 EOF
+docker-compose stop backend || true
+docker-compose rm -f backend || true
 docker-compose pull backend || true
-docker-compose --env-file .env_backend up --scale backend=2 -d --force-recreate backend || true
+docker-compose --env-file .env_backend up --scale backend=2 -d backend || true
