60549247d15368a89c382871f27dbe9b9329eb4b
std-017-006 <kyn07c0@yandex.ru>
2023-07-28 08:06:00 +0000

    edit deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -7,6 +7,8 @@ SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 EOF
 
+set +e
+
 BLUE_SERVICE="blue"
 GREEN_SERVICE="green"
 
@@ -29,25 +31,23 @@ echo "-----------------------------------"
 # Start the new environment
 echo "Starting inactive $INACTIVE_SERVICE container"
 docker-compose pull $INACTIVE_SERVICE || true
-docker compose --env-file .env_backend up -d --force-recreate $INACTIVE_SERVICE || true
+docker-compose --env-file .env_backend up -d --force-recreate $INACTIVE_SERVICE || true
 
 echo "Wait startup service $INACTIVE_SERVICE"
 for((i=1; i<=10; i++)); do
-  CONTAINER_IP=$(docker inspect --format='{{range $key, $value := .NetworkSettings.Networks}}{{if eq $key "'"$TRAEFIK_NETWORK"'"}}{{$value.IPAddress}}{{end}}{{end}}' "$INACTIVE_SERVICE" || true)
+  CONTAINER_IP=$(docker inspect --format='{{range $key, $value := .NetworkSettings.Networks}}{{if eq $key "'"sausage-store"'"}}{{$value.IPAddress}}{{end}}{{end}}' "$INACTIVE_SERVICE" || true)
   if [[ -z "$CONTAINER_IP" ]]; then
-    # The docker inspect command failed, so sleep for a bit and retry
-    sleep "$SLEEP_INTERVAL"
+    sleep 5
     continue
   fi
 
   HEALTH_CHECK_URL="http://$CONTAINER_IP:8080/actuator/health"
-  # N.B.: We use docker to execute curl because on macOS we are unable to directly access the docker-managed Traefik network.
-  if docker run --net $TRAEFIK_NETWORK --rm curlimages/curl:8.00.1 --fail --silent "$HEALTH_CHECK_URL" >/dev/null; then
+  if docker run --net sausage-store --rm curlimages/curl:8.00.1 --fail --silent "$HEALTH_CHECK_URL" >/dev/null; then
     echo "$INACTIVE_SERVICE is healthy"
     break
   fi
 
-  sleep "$SLEEP_INTERVAL"
+  sleep 5
 done
 
 
