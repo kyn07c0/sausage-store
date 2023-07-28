7925664629901bd9ea410a51617fff83fde78af8
std-017-006 <kyn07c0@yandex.ru>
2023-07-28 10:56:27 +0000

    edit deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -7,7 +7,7 @@ SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 EOF
 
-set +e
+set -e
 
 BLUE_SERVICE="blue"
 GREEN_SERVICE="green"
@@ -28,21 +28,25 @@ echo "Active service: $ACTIVE_SERVICE"
 echo "Inactive service: $INACTIVE_SERVICE"
 echo "-----------------------------------"
 
-# Start the new environment
+# Stop and remove second active container
+docker rm -f $INACTIVE_SERVICE || true 
+
+# Start inactive service
 echo "Starting inactive $INACTIVE_SERVICE container"
 docker-compose pull $INACTIVE_SERVICE || true
-docker-compose --env-file .env_backend up -d --force-recreate $INACTIVE_SERVICE || true
+docker-compose --env-file .env_backend up -d $INACTIVE_SERVICE || true
 
 echo "Wait startup service $INACTIVE_SERVICE"
 for((i=1; i<=10; i++)); do
-  CONTAINER_IP=$(docker inspect --format='{{range $key, $value := .NetworkSettings.Networks}}{{if eq $key "'"sausage-store"'"}}{{$value.IPAddress}}{{end}}{{end}}' "$INACTIVE_SERVICE" || true)
+  CONTAINER_IP=$(docker inspect --format='{{range $key, $value := .NetworkSettings.Networks}}{{if eq $key "'"sausage-store_sausage-store"'"}}{{$value.IPAddress}}{{end}}{{end}}' "$INACTIVE_SERVICE" || true)
   if [[ -z "$CONTAINER_IP" ]]; then
     sleep 5
     continue
   fi
 
   HEALTH_CHECK_URL="http://$CONTAINER_IP:8080/actuator/health"
-  if docker run --net sausage-store --rm curlimages/curl:8.00.1 --fail --silent "$HEALTH_CHECK_URL" >/dev/null; then
+  echo "HEALTH_CHECK_URL: $HEALTH_CHECK_URL"
+  if docker run --net sausage-store_sausage-store --rm curlimages/curl:8.00.1 --fail --silent "$HEALTH_CHECK_URL" >/dev/null; then
     echo "$INACTIVE_SERVICE is healthy"
     break
   fi
@@ -50,6 +54,34 @@ for((i=1; i<=10; i++)); do
   sleep 5
 done
 
+# If the new environment is not healthy within the timeout, stop it and exit with an error
+if ! docker run --net sausage-store_sausage-store --rm curlimages/curl:8.00.1 --fail --silent "$HEALTH_CHECK_URL" >/dev/null; then
+  echo "$INACTIVE_SERVICE did not become healthy within 60 seconds"
+  docker-compose down $INACTIVE_SERVICE
+  exit 1
+fi
 
-#docker-compose pull backend || true
-#docker-compose --env-file .env_backend up -d --force-recreate green || true
+# Check that Traefik recognizes the new container
+echo "Checking if Traefik recognizes $INACTIVE_SERVICE..."
+for ((i=1; i<=10; i++)); do
+  TRAEFIK_SERVER_STATUS=$(curl --fail --silent "$TRAEFIK_API_URL" | jq --arg container_ip "http://$CONTAINER_IP:8080" '.[] | select(.type == "loadbalancer") | select(.serverStatus[$container_ip] == "UP") | .serverStatus[$container_ip]')
+  if [[ -n "$TRAEFIK_SERVER_STATUS" ]]; then
+    echo "Traefik recognizes $INACTIVE_SERVICE as healthy"
+    break
+  fi
+
+  sleep 5
+done
+
+# If Traefik does not recognize the new container within the timeout, stop it and exit with an error
+#if [[ -z "$TRAEFIK_SERVER_STATUS" ]]; then
+#  echo "Traefik did not recognize $INACTIVE_SERVICE within 60 seconds"
+#  docker-compose stop --timeout=30 $INACTIVE_SERVICE
+#  exit 1
+#fi
+
+# Set Traefik priority label to 0 on the old service and stop the old environment if it was previously running
+if [[ -n "$ACTIVE_SERVICE" ]]; then
+  echo "Stopping $ACTIVE_SERVICE container"
+  docker-compose down $ACTIVE_SERVICE
+fi
