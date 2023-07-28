a101e82548bc71d66a83b814a163f42816079b69
std-017-006 <kyn07c0@yandex.ru>
2023-07-28 07:05:59 +0000

    edit docker-compose.yaml

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -6,5 +6,25 @@ SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}
 SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 EOF
+
+BLUE_SERVICE="blue"
+GREEN_SERVICE="green"
+
+# Find which service is currently active
+if docker ps --format "{{.Names}}" | grep -q "$BLUE_SERVICE"; then
+  ACTIVE_SERVICE=$BLUE_SERVICE
+  INACTIVE_SERVICE=$GREEN_SERVICE
+elif docker ps --format "{{.Names}}" | grep -q "$GREEN_SERVICE"; then
+  ACTIVE_SERVICE=$GREEN_SERVICE
+  INACTIVE_SERVICE=$BLUE_SERVICE
+else
+  ACTIVE_SERVICE=""
+  INACTIVE_SERVICE=$BLUE_SERVICE
+fi
+
+echo $ACTIVE_SERVICE
+echo $INACTIVE_SERVICE
+
+
 docker-compose pull backend || true
 docker-compose --env-file .env_backend up -d --force-recreate green || true
